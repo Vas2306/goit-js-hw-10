d4ceceb3a4bfe89e2d39304f4982a6277aa37ace
Vas2306 <vasilisa230679@gmail.com>
2023-03-07 12:34:00 +0100

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,54 @@
 import './css/styles.css';
 
+import debounce from 'lodash.debounce';
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+import { fetchCountries } from './js/fetchCountries';
+import { showCountryList, showCountryCard } from './js/template';
+import { refs } from './js/refs-elements';
+
 const DEBOUNCE_DELAY = 300;
+
+refs.inputSearch.addEventListener('input', debounce(onSearch, DEBOUNCE_DELAY));
+
+function onSearch(e) {
+  e.preventDefault();
+  const search = e.target.value;
+  console.log(e.target.value);
+  if (search.trim() === '') {
+    refs.countryList.innerHTML = '';
+    refs.countryInfo.innerHTML = '';
+    return;
+  }
+
+  fetchCountries(search.trim())
+    .then(countries => {
+      if (countries.length > 10) {
+        Notify.info(
+          'Too many matches found. Please enter a more specific name.'
+        );
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = '';
+        return;
+      }
+
+      if (countries.length > 1 && countries.length <= 10) {
+        const markup = countries.map(country => showCountryList(country));
+        refs.countryList.innerHTML = markup.join('');
+        refs.countryInfo.innerHTML = '';
+      }
+
+      if (countries.length === 1) {
+        const cardMarcup = countries.map(country => showCountryCard(country));
+        refs.countryList.innerHTML = '';
+        refs.countryInfo.innerHTML = cardMarcup.join('');
+      }
+    })
+
+    .catch(error => {
+      Notify.failure('Oops, there is no country with that name');
+      refs.countryList.innerHTML = '';
+      refs.countryInfo.innerHTML = '';
+      return error;
+    });
+}
---
+++ src/js/fetchCountries.js
@@ -0,0 +1,14 @@
+const BASE_URL = 'https://restcountries.com/v3.1';
+const FIELDS = `?fields=name,capital,population,flags,languages`;
+
+export function fetchCountries(name) {
+
+  return fetch(`${BASE_URL}/name/${name}${FIELDS}`).then(
+    response => {
+      if (!response.ok) {
+        throw new Error(response.status);
+      }
+      return response.json();
+    }
+  );
+}
